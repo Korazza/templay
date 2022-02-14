8942b3efd12074543111acfe5ece13aea9cde4ed
Korazza <corattiroberto@gmail.com>
2022-02-14 22:32:13 +0100

    Get config

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/Korazza/templay/cmd"
+import (
+	"github.com/Korazza/templay/cmd"
+	"github.com/Korazza/templay/config"
+	"github.com/Korazza/templay/logger"
+)
 
 func main() {
-	cmd.Execute()
+	var c config.Config
+
+	if err := c.SetPwd(); err != nil {
+		logger.Error.Fatal(err)
+	}
+
+	if err := c.Load(); err != nil {
+		logger.Warning.Print(err)
+	}
+
+	if err := c.Validate(); err != nil {
+		logger.Error.Fatal(err)
+	}
+
+	cmd.Execute(c)
 }
