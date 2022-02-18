896ad42b335d1110e30ca03680a72877e9cdfaa4
Korazza <corattiroberto@gmail.com>
2022-02-18 18:13:44 +0100

    Refactor generator

--- generator/generator.go
+++ generator/generator.go
@@ -1,77 +1,30 @@
 package generator
 
 import (
+	"bytes"
 	"fmt"
-	"html/template"
 	"io/ioutil"
 	"os"
 	"path"
 	"strings"
-	"text/template/parse"
+	"text/template"
+
+	"github.com/Korazza/templay/config"
 )
-
-func listTemplFields(t *template.Template) []string {
-	return listNodeFields(t.Tree.Root)
-}
-
-func listNodeFields(node parse.Node) []string {
-	var res []string
-	if node.Type() == parse.NodeAction {
-		field := strings.ReplaceAll(node.String(), " | _html_template_htmlescaper", "")
-		res = append(res, field[3:len(field)-2])
-	}
-	if ln, ok := node.(*parse.ListNode); ok {
-		for _, n := range ln.Nodes {
-			res = append(res, listNodeFields(n)...)
-		}
-	}
-	return res
-}
-
-func validateTemplay(t *template.Template, vars map[string]interface{}) error {
-	tvars := listTemplFields(t)
-	var valid int
-	var validvars []int
-	for _, k := range tvars {
-		if _, ok := vars[k]; ok {
-			validvars = append(validvars, valid)
-			valid++
-		}
-	}
-	if valid != len(tvars) {
-		for i := range validvars {
-			tvars[i] = tvars[len(tvars)-1]
-			tvars = tvars[:len(tvars)-1]
-		}
-		var errors []string
-		for _, v := range tvars {
-			errors = append(errors, fmt.Sprintf("%3s %s", "-", v))
-		}
-		return fmt.Errorf("templay \"%s\" requires more variables: \n%v", t.Name(), strings.Join(errors, "\n"))
-	}
-	return nil
-}
 
 func ParseFile(src, dst string, vars map[string]interface{}) error {
 	var (
 		err     error
-		srcfd   *os.File
 		dstfd   *os.File
 		srcinfo os.FileInfo
 		templay *template.Template
 	)
 
-	if srcfd, err = os.Open(src); err != nil {
-		return err
-	}
-	defer srcfd.Close()
+	templay = template.Must(template.ParseFiles(src)).Option("missingkey=error")
+	buf := &bytes.Buffer{}
 
-	templay, err = template.ParseFiles(src)
+	err = templay.Execute(buf, vars)
 	if err != nil {
-		return err
-	}
-
-	if err = validateTemplay(templay, vars); err != nil {
 		return err
 	}
 
@@ -80,8 +33,7 @@ func ParseFile(src, dst string, vars map[string]interface{}) error {
 	}
 	defer dstfd.Close()
 
-	err = templay.Execute(dstfd, vars)
-	if err != nil {
+	if _, err = dstfd.Write(buf.Bytes()); err != nil {
 		return err
 	}
 
@@ -114,8 +66,11 @@ func ParseDirectory(src, dst string, vars map[string]interface{}) error {
 	var errors []string
 
 	for _, fd := range fds {
+		if !fd.IsDir() && !strings.HasSuffix(fd.Name(), config.TEMPLAY_EXTENSION) {
+			return fmt.Errorf("templay files need \"%s\" extension", config.TEMPLAY_EXTENSION)
+		}
 		srcfp := path.Join(src, fd.Name())
-		dstfp := path.Join(dst, fd.Name())
+		dstfp := path.Join(dst, strings.TrimSuffix(fd.Name(), config.TEMPLAY_EXTENSION))
 
 		if fd.IsDir() {
 			if err = ParseDirectory(srcfp, dstfp, vars); err != nil {
@@ -128,8 +83,8 @@ func ParseDirectory(src, dst string, vars map[string]interface{}) error {
 		}
 	}
 	if len(errors) > 0 {
-		errMsg := strings.Join(errors, "\n\n")
-		return fmt.Errorf("\n%s", errMsg)
+		errMsg := strings.Join(errors, fmt.Sprintf("\n%2s", ""))
+		return fmt.Errorf("\n%2s%s", "", errMsg)
 	}
 	return nil
 }
